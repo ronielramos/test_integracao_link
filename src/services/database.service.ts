56a58f90d3bc18ccb2659d22bce9368e712f4471
Roniel <roniel_ramos@hotmail.com>
2019-11-19 13:36:18 -0300

    feat: criar conexao com o BD e exportar collections

---
+++ src/services/database.service.ts
@@ -0,0 +1,23 @@
+import { Collection, Db } from 'mongodb'
+import DbClient = require('./infra/database.infra');
+
+let collections: {
+  pedido: Collection;
+}
+
+const bdConectado = (db: unknown): db is Db => {
+  return db ? (db as Db).collection !== undefined : false
+}
+
+export const getCollection = (nome: 'pedido') => {
+  if (collections && collections[nome]) return collections[nome]
+  if (!bdConectado(DbClient.db)) throw new Error('Falha de conexão com o BD')
+
+  const db: Db = DbClient.db
+
+  collections = {
+    pedido: db.collection('pedido')
+  }
+
+  return collections[nome]
+}
