a6a3598c71d42a64cd28d7672c3e95e4693042a5
Roniel <roniel_ramos@hotmail.com>
2019-11-19 13:36:00 -0300

    feat: criar pedido no servico bling

---
+++ src/services/bling.service.ts
@@ -0,0 +1,30 @@
+import { executar } from './external-request'
+import { Pedido, RetornoCriacaoPedido } from '../teste-integracao'
+import { toXML } from './utils'
+
+export const criarPedido = async (
+  pedido: Pedido
+): Promise<RetornoCriacaoPedido> => {
+  const pedidoFormatado = {
+    pedido: {
+      cliente: pedido.cliente,
+      item: pedido.itens
+    }
+  }
+
+  const pedidoXML = toXML(pedidoFormatado)
+  const XML = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    ${pedidoXML}
+  `
+
+  const resultado = await executar({
+    method: 'POST',
+    url: 'https://bling.com.br/Api/v2/pedido/json/',
+    qs: {
+      apikey: process.env.BLING_API_KEY,
+      xml: XML
+    }
+  })
+  return resultado.body as RetornoCriacaoPedido
+}
