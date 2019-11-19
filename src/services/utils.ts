7ec976b14cb4b7b93d4cf394f39b5d10761c3617
Roniel <roniel_ramos@hotmail.com>
2019-11-19 13:37:52 -0300

    feat: converter JSON para XML e vice-versa

---
+++ src/services/utils.ts
@@ -0,0 +1,6 @@
+import convert from 'xml-js'
+
+export const toXML = (json: Object) =>
+  convert.js2xml(json, { compact: true, spaces: 2 })
+export const toJson = (xmlString: string) =>
+  convert.xml2js(xmlString, { compact: true })
