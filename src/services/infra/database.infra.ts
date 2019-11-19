25e648e8f11e83f4da82206bd99a8fcd38a65854
Roniel <roniel_ramos@hotmail.com>
2019-11-19 13:38:07 -0300

    feat: Criar conexão com o BD

---
+++ src/services/infra/database.infra.ts
@@ -0,0 +1,38 @@
+import { MongoClient, Db } from 'mongodb'
+
+class DbClient {
+  public db: Db | undefined;
+  public connecting: boolean = false;
+
+  constructor () {
+    this.connect()
+  }
+
+  public async connect () {
+    try {
+      if (this.connecting) return
+      if (this.db) return this.db
+
+      this.connecting = true
+
+      const database = await MongoClient.connect(process.env.MONGODB_URI || '', {
+        poolSize: 10,
+        reconnectInterval: 2000,
+        autoReconnect: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+
+      this.db = database.db('db_teste_integracao_link')
+      this.connecting = false
+
+      console.log('MongoDB connected')
+
+      return this.db
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+
+export = new DbClient();
