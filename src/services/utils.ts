45c98bed567a28f16cc86aa78df64d873e1ab5cd
Roniel <roniel_ramos@hotmail.com>
2019-12-29 17:29:55 -0300

    teste do husky e lint-staged

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -1,6 +1,4 @@
 import convert from 'xml-js'
 
-export const toXML = (json: Object) =>
-  convert.js2xml(json, { compact: true, spaces: 2 })
-export const toJson = (xmlString: string) =>
-  convert.xml2js(xmlString, { compact: true })
+export const toXML = (json: Object) => convert.js2xml(json, { compact: true, spaces: 2 })
+export const toJson = (xmlString: string) => convert.xml2js(xmlString, { compact: true })
